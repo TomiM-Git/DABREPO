00e613ee94a9c44150ae38b0c16140bdcfc5c14e
mathrisk <tvm21270@gmail.com>
2023-04-22 21:33:33 -0300

    [ACTUALIZACIÓN IMPORTANTE] Se incorpora y actualiza código CalcularVo.py y Parametricas.py  para contrastar mediciones experimentales con datos teoricós. Se modifica código SalidaVariable.py para mejorar su utilidad en el contexto de diseño y complemento gráfico del informe del proyecto. Se reordenan las funciones acorde a la instancia de diseño. Se incorporan comentarios. Pendiente completar comentarios para versión final de la plataforma.

--- PlataformaDiseño/CalcularCtes.py
+++ PlataformaDiseño/CalcularCtes.py
@@ -1,154 +1,139 @@
-def Calcular(Vi,Vo,n,D,L,fs):
+#****   Función que calcula e imprime en consola los resultados intermedios y finales
+# del procedimiento realizado en el Capítulo 3 del Marco Teórico para hallar expresiones 
+# de corriente media y tensión media de salida del convertidor.    ****
+def calcular(Vi,Vo,n,D,L,fs,Rl): 
+
     import math
-    import numpy as np
-    PI=3.14159265
-    phi=D*PI    #Desfase entre puentes en radianes
-    Ts=1/(2*fs) #Semiperiodo de conmutacion
+    PI=3.14159265   #Constante PI.
+    phi=D*PI        #Desfase entre puentes en radianes.
+    Ts=1/(2*fs)     #Semiperiodo de conmutación.
 
-    print("********************************************************")
-    print("********* CORRIENTES EN CONDICION MAS EXIGENTE *********")
-    print("********************************************************")
+    print("**********************************************************************")
+    print("************* CORRIENTES DADOS LOS SIGUIENTES PARÁMETROS *************")
+    print("**********************************************************************")
 
-#*****************************************************************************
-    print("Parametros:\n")
-    print("Tension de entrada                   Vi = ",round(Vi,2)," [V]")
-    print("Tension de salida                    Vo = ",round(Vo,2)," [V]")
-    print("Relacion de transformacion           n = ",round(n,2))
-    print("Desfase entre puentes en radianes    phi = ",round(phi,2),"rad")
-    print("Desfase porcentual entre puentes     D = ",round(D,2))
-    print("Inductancia equivalente total        L = ",round(L*1e6,2),"[uHy]")
-    print("Frecuencia de conmutacion            fs = ",round(fs*(1e-3),2),"[kHz]")
+#**** Presentación de valores que toman los parámetros. ****
+    print("**************************** Parámetros: *****************************")
+    print("Tensión de entrada:                   Vi = ",round(Vi,2)," [V]")
+    print("Tensión de salida:                    Vo = ",round(Vo,2)," [V]")
+    print("Relación de transformación:           n = ",round(n,2))
+    print("Desfase entre puentes en radianes:    phi = ",round(phi,2),"rad")
+    print("Desfase porcentual entre puentes:     D = ",round(D,2))
+    print("Inductancia equivalente total:        L = ",round(L*1e6,2),"[uHy]")
+    print("Frecuencia de conmutación:            fs = ",round(fs*(1e-3),2),"[kHz]")
+    print("Resistencia de carga:                 Rl = ",round(Rl,2),"[ohm]")
+
+#****       ****
+    print("\n*** Intervalos de tiempo y variaciones de corriente en inductor: *****")
+    print("     (dt1 = dt3 = D*Ts)  ;  (dt2 = dt4 = (1-D)*Ts)      ")
+    print("Duración de intervalo        [0:phi]: dt1 = ",round(D*Ts*1e6,2)," [us]")
+    print("Duración de intervalo       [phi:PI]: dt2 = ",round((1-D)*Ts*1e6,2)," [us]")
+    print("Duración de intervalo    [PI:PI+phi]: dt3 = ",round(D*Ts*1e6,2)," [us]")
+    print("Duración de intervalo  [PI+phi:2*PI]: dt4 = ",round((1-D)*Ts*1e6,2)," [us]")
+    print("           (dI1 = -dI3)  ;  (dI2 = -dI4)      ")
+    dI1=(Vi+Vo/n)*(D*Ts)/L          #Pendiente de corriente instantánea en intervalo [0:phi].
+    dI2=(Vi-Vo/n)*((1-D)*Ts)/L      #Pendiente de corriente instantánea en intervalo [phi:PI].
+    dI3=(-Vi-Vo/n)*(D*Ts)/L         #Pendiente de corriente instantánea en intervalo [PI:PI+phi].
+    dI4=(-Vi+Vo/n)*((1-D)*Ts)/L     #Pendiente de corriente instantánea en intervalo [PI+phi:2*PI].
+    print("Variacion en intervalo       [0:phi]: dI1 = ",round(dI1,2)," [A]")
+    print("Variacion en intervalo      [phi:PI]: dI2 = ",round(dI2,2)," [A]")
+    print("Variacion en intervalo   [PI:PI+phi]: dI3 = ",round(dI3,2)," [A]")
+    print("Variacion en intervalo [PI+phi:2*PI]: dI4 = ",round(dI4,2)," [A]")
 
 
-    print("\nVariaciones absolutas de corriente por intervalo en PRIMARIO e INDUCTOR:\n")
+#****       ****
+    print("\n*** Corrientes instantáneas en puntos de inflexión: ******************")
+    IA=-(dI1+dI2)/2                             #Corriente instantánea en primario en 0 radianes.
+    IB=IA+dI1                                   #Corriente instantánea en primario en phi radianes.
+    IC=IB+dI2                                   #Corriente instantánea en primario en PI radianes.
+    ID=IC+dI3                                   #Corriente instantánea en primario en PI+phi radianes.
+    print("Fase=0                                IA = ",round(IA,2)," [A]")
+    print("Fase=phi                              IB = ",round(IB,2)," [A]")
+    print("Fase=PI                               IC = ",round(IC,2)," [A]")
+    print("Fase=PI+phi                           ID = ",round(ID,2)," [A]")
+    IA_s=IA/n                                   #Corriente instantánea en secundario en 0 radianes.
+    IB_s=IB/n                                   #Corriente instantánea en secundario en phi radianes.
+    IC_s=IC/n                                   #Corriente instantánea en secundario en PI radianes.
+    ID_s=ID/n                                   #Corriente instantánea en secundario en PI+phi radianes.
+    print("Fase=0                                IA_s = ",round(IA_s,2)," [A]")
+    print("Fase=phi                              IB_s = ",round(IB_s,2)," [A]")
+    print("Fase=PI                               IC_s = ",round(IC_s,2)," [A]")
+    print("Fase=PI+phi                           ID_s = ",round(ID_s,2)," [A]")
 
-    dI1=(Vi+Vo/n)*(D*Ts)/L          #Pendiente de corriente instantanea en intervalo [0:phi]
-    dI2=(Vi-Vo/n)*((1-D)*Ts)/L      #Pendiente de corriente instantanea en intervalo [phi:PI]
-    dI3=(-Vi-Vo/n)*(D*Ts)/L         #Pendiente de corriente instantanea en intervalo [PI:PI+phi]
-    dI4=(-Vi+Vo/n)*((1-D)*Ts)/L     #Pendiente de corriente instantanea en intervalo [PI+phi:2*PI]
-    
-    print("Intervalo [0:phi]               delta_I1 = ",round(dI1,1)," [A]")
-    print("Intervalo [phi:PI]              delta_I2 = ",round(dI2,1)," [A]")
-    print("Intervalo [PI:PI+phi]           delta_I3 = ",round(dI3,1)," [A]")
-    print("Intervalo [PI+phi:2*PI]         delta_I4 = ",round(dI4,1)," [A]")
+#****       ****
+    alpha = math.atan(phi/dI1)                  #Ángulo geometrico para calculo de integrales triangulares entre 0 y phi.
+    theta_x = abs(IA)*math.tan(alpha)           #Fase en abscisas de cruce por cero de la corriente en el inductor.
+    print("\n*** Ángulos para cálculos con figuras geométricas: *******************")
+    print("Ángulo geométrico proyección eje x:   alpha = ",round(alpha,3)," rad")
+    print("Fase de cruce por cero:               theta_x = ",round(theta_x,3),"rad")
 
-#*****************************************************************************
-    print("\nCorrientes instantaneas en PRIMARIO e INDUCTOR en puntos de inflexion:\n")
-
-    I0=-(dI1+dI2)/2
-    I1=I0+dI1
-    I2=I1+dI2
-    I3=I2+dI3
-    
-    print("Fase=0                               IA = ",round(I0,1)," [A]")
-    print("Fase=phi                             IB = ",round(I1,1)," [A]")
-    print("Fase=PI                              IC = ",round(I2,1)," [A]")
-    print("Fase=PI+phi                          ID = ",round(I3,1)," [A]")
-
-    print("\nCorrientes instantaneas en el SECUNDARIO en puntos de inflexion:\n")
-
-    I0_s=I0/n
-    I1_s=I1/n
-    I2_s=I2/n
-    I3_s=I3/n
-
-    print("Fase=0                               IA_s = ",round(I0_s,1)," [A]")
-    print("Fase=phi                             IB_s = ",round(I1_s,1)," [A]")
-    print("Fase=PI                              IC_s = ",round(I2_s,1)," [A]")
-    print("Fase=PI+phi                          ID_s = ",round(I3_s,1)," [A]")
-#*****************************************************************************
-
-    alpha = math.atan(phi/dI1)
-    theta_x = abs(I0)*math.tan(alpha)
-    print("\nAngulos para calculo de corrientes eficaces y corriente media:\n")
-    print("Angulo geometrico proyeccion ejex alpha = ",round(alpha,3)," rad")
-    print("Fase de cruce por cero theta_x = ",round(theta_x,3),"rad")
-
-    Iav_out1=((I1_s+I2_s)*(1-D)/2)
-    Iav_out2=I2_s*theta_x/(2*PI)
-    Iav_out3=I3_s*(phi-theta_x)/(2*PI)
-    Iav_out=Iav_out1+Iav_out2+Iav_out3
-    print("\nCorriente media de salida del convertidor:   Iav_out = ",round(Iav_out,2)," [A]")
-
-#*****************************************************************************
-    print("\nCalculos de corrientes eficaces:\n")
+#****       ****
+    print("\n*** Cálculos de corrientes eficaces: *********************************")
  
-    Ief1=(abs(I0)/theta_x)*math.sqrt((theta_x**3)/(3*PI))
-    Ief2=(abs(I1)/(phi-theta_x))*math.sqrt(((phi-theta_x)**3)/(3*PI))
+    Ief1=(abs(IA)/theta_x)*math.sqrt((theta_x**3)/(3*PI))               #Corriente eficaz triangular entre [0:theta_x] y [PI:PI+theta_x].
+    Ief2=(abs(IB)/(phi-theta_x))*math.sqrt(((phi-theta_x)**3)/(3*PI))   #Corriente eficaz triangular entre [theta_x:phi] y [PI+theta_x:PI+phi].
+    Ief3=math.sqrt(((IB)**2)*(PI-phi)/(PI))                             #Corriente eficaz rectangular entre [phi:PI].
+    Ief4=((abs(IC)-abs(IB))/(PI-phi))*math.sqrt(((PI-phi)**3)/(3*PI))   #Corriente eficaz triangular entre [phi:PI].
+    IefL=math.sqrt(Ief1**2+Ief2**2+(Ief3+Ief4)**2)  #Suma vectorial para obtener corriente eficaz total en el inductor y devanado primario.
 
-    Ief3=math.sqrt(((I1)**2)*(PI-phi)/(PI))
-    Ief4=((abs(I2)-abs(I1))/(PI-phi))*math.sqrt(((PI-phi)**3)/(3*PI))
+    print("En PRIMARIO [P] e INDUCTOR [L]        IefL = ",round(IefL,2)," [A]")
+    Id_ef_p=IefL/math.sqrt(2) 
+    print("En MOSFET del puente P1               Id_ef_p = ",round(Id_ef_p,2)," [A]")
+    Ief_s=IefL/n 
+    print("En el SECUNDARIO [S]                  Ief_s = ",round(Ief_s,2)," [A]")
+    Id_ef_s=Ief_s/math.sqrt(2) 
+    print("En MOSFET del puente P2               Id_ef_s = ",round(Id_ef_s,2)," [A]")
 
-    IefL=math.sqrt(Ief1**2+Ief2**2+(Ief3+Ief4)**2)
+#****       ****
+    Io1=((IB_s+IC_s)*(1-D)/2)
+    Io2=IC_s*theta_x/(2*PI)
+    Io3=ID_s*(phi-theta_x)/(2*PI)
+    Io=Io1+Io2+Io3
+    print("\n*** Corriente media de salida:        Io = ",round(Io,2)," [A] ****************")
+#****       ****
+    Rl=Vo/Io
+    print("*** Carga resistiva requerida:        Rl = Vo/Io = ",round(Rl,0)," [ohm] ******")
 
-    #print("\nAuxiliar 1:        Ief1 = ",Ief1)
-    #print("Auxiliar 2:        Ief2 = ",Ief2)
-    #print("Auxiliar 1:        Ief3 = ",Ief3)
-    #print("Auxiliar 2:        Ief4 = ",Ief4)
-    print("En PRIMARIO [P] e INDUCTOR [L]       IefL = ",round(IefL,2)," [A]")
-
-    Ief_ce_p=IefL/math.sqrt(2)
-    print("En IGBT del PRIMARIO [P]             Ief_ce_p = ",round(Ief_ce_p,2)," [A]")
-
-    Ief_s=IefL/n
-    print("En el SECUNDARIO[S]                  Ief_s = ",round(Ief_s,2)," [A]")
-
-    Ief_ce_s=Ief_s/math.sqrt(2)
-    print("En IGBT del SECUNDARIO[S]            Ief_ce_s = ",round(Ief_ce_s,2)," [A]")
-
-#    return (IefL,Ief_s)
-
-#******************************************************************************
-def calcSinPrint(Vi,Vo,n,D,L,fs): #Funcion que retorna los valores, sin imprimir en consola
+#****    Función que retorna los valores de corriente eficaz en devanados del transformador HFT.   ****
+def calcSinPrint(Vi,Vo,n,D,L,fs):
 
     import math
-    PI=3.14159265
-    phi=D*PI
-    Ts=1/(2*fs)
 
-    dI1=(Vi+Vo/n)*(D*Ts)/L          #Pendiente de corriente instantanea en intervalo [0:phi]
-    dI2=(Vi-Vo/n)*((1-D)*Ts)/L      #Pendiente de corriente instantanea en intervalo [phi:PI]
-    dI3=(-Vi-Vo/n)*(D*Ts)/L         #Pendiente de corriente instantanea en intervalo [PI:PI+phi]
-    dI4=(-Vi+Vo/n)*((1-D)*Ts)/L     #Pendiente de corriente instantanea en intervalo [PI+phi:2*PI]
+    PI=3.14159265                       #Constante PI.
+    phi=D*PI                            #Desfase entre puentes
+    Ts=1/(2*fs)                         #Semiperiodo de conmutación.
 
-    I0=-(dI1+dI2)/2                 #Corriente instantanea en primario en 0 radianes
-    I1=I0+dI1                       #Corriente instantanea en primario en phi radianes
-    I2=I1+dI2                       #Corriente instantanea en primario en PI radianes
-    I3=I2+dI3                       #Corriente instantanea en primario en PI+phi radianes
+    dI1=(Vi+Vo/n)*(D*Ts)/L              #Pendiente de corriente instantánea en intervalo [0:phi].
+    dI2=(Vi-Vo/n)*((1-D)*Ts)/L          #Pendiente de corriente instantánea en intervalo [phi:PI].
+    dI3=(-Vi-Vo/n)*(D*Ts)/L             #Pendiente de corriente instantánea en intervalo [PI:PI+phi].
+    dI4=(-Vi+Vo/n)*((1-D)*Ts)/L         #Pendiente de corriente instantánea en intervalo [PI+phi:2*PI].
 
-    I0_s=I0/n                       #Corriente instantanea en secundario en 0 radianes
-    I1_s=I1/n                       #Corriente instantanea en secundario en phi radianes
-    I2_s=I2/n                       #Corriente instantanea en secundario en PI radianes
-    I3_s=I3/n                       #Corriente instantanea en secundario en PI+phi radianes
+    IA=-(dI1+dI2)/2                     #Corriente instantánea en primario en 0 radianes.
+    IB=IA+dI1                           #Corriente instantánea en primario en phi radianes.
+    IC=IB+dI2                           #Corriente instantánea en primario en PI radianes.
+    ID=IC+dI3                           #Corriente instantánea en primario en PI+phi radianes.
+    IA_s=IA/n                           #Corriente instantánea en secundario en 0 radianes.
+    IB_s=IB/n                           #Corriente instantánea en secundario en phi radianes.
+    IC_s=IC/n                           #Corriente instantánea en secundario en PI radianes.
+    ID_s=ID/n                           #Corriente instantánea en secundario en PI+phi radianes.
 
-    alpha = math.atan(phi/dI1)          #Angulo geometrico para calculo de integrales triangulares entre 0 y phi
-    theta_x = abs(I0)*math.tan(alpha)   #Fase en abscisas de cruce por cero de la corriente en el inductor
-
-#    print("alpha = ",alpha)
-#    print("theta_x = ",theta_x)
-#    print("phi = ",phi)
-#    print("I0 =",I0)
-#    print("I1 =",I1)
-#    print("I2 =",I2)
-#    print("I3 =",I3)
-
-
-    #Corriente eficaz triangular entre [0:theta_x] y [PI:PI+theta_x]
-    Ief1=(abs(I0)/theta_x)*math.sqrt((theta_x**3)/(3*PI))  
-    #Corriente eficaz triangular entre [theta_x:phi] y [PI+theta_x:PI+phi]
-    Ief2=(abs(I1)/(phi-theta_x))*math.sqrt(((phi-theta_x)**3)/(3*PI)) 
-    #Corriente eficaz rectangular entre [phi:PI]
-    Ief3=math.sqrt(((I1)**2)*(PI-phi)/(PI))
-    #Corriente eficaz triangular entre [phi:PI]
-    Ief4=((abs(I2)-abs(I1))/(PI-phi))*math.sqrt(((PI-phi)**3)/(3*PI))
-    #Suma vectorial para obtener corriente eficaz total en el inductor y devanado primario
-    IefL=math.sqrt(Ief1**2+Ief2**2+(Ief3+Ief4)**2)
-
-    Ief_ce_p=IefL/math.sqrt(2)      #Corriente eficaz en llaves del primario
-    Ief_s=IefL/n                    #Corriente eficaz en devanado secundario
-    Ief_ce_s=Ief_s/math.sqrt(2)     #Corriente eficaz en llaves del secundario
-
-    return (IefL,Ief_s)     #Retorno corriente en inductor/primario y en secundario
-
-
+    alpha = math.atan(phi/dI1)          #Ángulo geométrico para calculo de integrales triangulares entre 0 y phi.
+    theta_x = abs(IA)*math.tan(alpha)   #Fase en abscisas de cruce por cero de la corriente en el inductor.
+#    print("alpha=",alpha)
+#    print("phi=",phi)
+#    print("theta_x=",theta_x)
+    Ief1=(abs(IA)/theta_x)*math.sqrt((theta_x**3)/(3*PI))               #Corriente eficaz triangular entre [0:theta_x] y [PI:PI+theta_x].
+    Ief2=(abs(IB)/(phi-theta_x))*math.sqrt(((phi-theta_x)**3)/(3*PI))   #Corriente eficaz triangular entre [theta_x:phi] y [PI+theta_x:PI+phi].
+    Ief3=math.sqrt(((IB)**2)*(PI-phi)/(PI))                             #Corriente eficaz rectangular entre [phi:PI].
+    Ief4=((abs(IC)-abs(IB))/(PI-phi))*math.sqrt(((PI-phi)**3)/(3*PI))   #Corriente eficaz triangular entre [phi:PI].
+    IefL=math.sqrt(Ief1**2+Ief2**2+(Ief3+Ief4)**2)      #Suma vectorial para obtener corriente eficaz total en el inductor y devanado primario.
+    Id_ef_p=IefL/math.sqrt(2)     #Corriente eficaz en llaves del primario.
+    Ief_s=IefL/n                    #Corriente eficaz en devanado secundario.
+    #Id_ef_s=Ief_s/math.sqrt(2)    #Corriente eficaz en llaves del secundario.
+#****       ****
+    Io1=((IB_s+IC_s)*(1-D)/2)
+    Io2=IC_s*theta_x/(2*PI)
+    Io3=ID_s*(phi-theta_x)/(2*PI)
+    Io=Io1+Io2+Io3
+    return (Id_ef_p,Io)             #Retorno corriente en inductor/primario y en secundario.
 
--- PlataformaDiseño/SalidaVariable.py
+++ PlataformaDiseño/SalidaVariable.py
@@ -1,44 +1,88 @@
-def graficarCorriente(Vi_n,Vo_min,Vo_max,n,L,fs,pts):
+def graficarCorriente(Vi,Vo_inf,Vo_sup,n,L,fs,Id_max,Vo_max,MODO):
 
     import numpy as np
     import CalcularCtes
     import matplotlib.pyplot as plt
-    PI=3.14159265
-    D_var=0.1       #Desfase porcentual inicial
-    D_step=0.1      #Paso del desfase porcentual
 
-    Vo_var=Vo_min   #Tension de salida inicial
-    Vo_step=(Vo_max-Vo_min)/(pts-1) #Paso de tension de salida
+    pts = int(round(((Vo_sup-Vo_inf)*0.5),0)+1)        #Número de puntos a graficar
 
-    plt.figure(figsize=[14,7]) #Define tamaño de grafica
-    for j in (range(5)):    #Se ejecuta una vez por cada desfase porcentual a graficar
-        x = np.zeros((pts,1),dtype=float)#Vector para voltajes de salida
-        y = np.zeros((pts,1),dtype=float)#Vector para valores de corriente eficaz en inductor
-        for i in (range(pts)):  #Se ejecuta la funcion para completar los vectores
-            x[i] = Vo_var       #Se guarda el valor vector x
-            cte_graf=(CalcularCtes.calcSinPrint(Vi_n,Vo_var,n,D_var,L,fs))[0]
-            y[i] = cte_graf #Se guarda valor vector
-#            print(cte_graf,Vo_var,D_var)
-            Vo_var = Vo_var+Vo_step #Nuevo paso para proxima iteracion
-        if(j==0): #Selecciona el grafico segun el desfase porcentual a graficar
-            plt.plot(x,y,'gs-',linewidth=2.0,label=r'$\phi='+str(D_var*180)+'$')
-        elif(j==1):
-            plt.plot(x,y,'ro-',linewidth=2.0,label=r'$\phi='+str(D_var*180)+'$')
-        elif(j==2):
-            plt.plot(x,y,'k^-',linewidth=2.0,label=r'$\phi='+str(D_var*180)+'$')
-        elif(j==3):
-            plt.plot(x,y,'g^-',linewidth=2.0,label=r'$\phi='+str(D_var*180)+'$')
-        elif(j==4):
-            plt.plot(x,y,'rs-',linewidth=2.0,label=r'$\phi='+str(D_var*180)+'$')
-        elif(j==5):
-            plt.plot(x,y,'ko-',linewidth=2.0,label=r'$\phi='+str(D_var*180)+'$')
-        Vo_var=Vo_min #Se reinicializa el valor del voltaje minimo a graficar
-        D_var=D_var+D_step #Se incrementa el desfase porcentual para proxima grafica
+    Vo_var=Vo_inf       #Tensión de salida inicial.
+    Vo_step=(Vo_sup-Vo_inf)/(pts-1) #Paso de tensión de salida.
 
+    Id_max_vec=[Id_max,Id_max]
+    Vo_vec=[Vo_inf,Vo_sup]
+
+    D_var=0.1           #Desfase porcentual inicial. D=0.1 equivale a phi=18°
+    D_step=0.05         #Incremento del desfase porcentual para graficar parametricas.
+
+    fig = plt.figure(figsize=[14,10]) #Define tamaño de grafica
+    ax = fig.add_subplot(1,1,1)
+    if(MODO==2):
+        plt.plot(Vo_vec,Id_max_vec,'k^-',linewidth=2.0,label=r'$ Id,max = '+str(Id_max)+'$')
+    for j in (range(9)):                        #Se ejecuta una vez por cada desfase porcentual a graficar.
+        x = np.zeros((pts,1),dtype=float)       #Vector para tensiones de salida.
+        y1 = np.zeros((pts,1),dtype=float)      #Vector para valores de corriente eficaz en inductor.
+        y2 = np.zeros((pts,1),dtype=float)      #Vector para valores de corriente eficaz en mosfets P1.
+        for i in (range(pts)):  #Se ejecuta la función para completar los vectores.
+            x[i] = Vo_var       #Se guarda el valor vector x.
+            y1[i]=(CalcularCtes.calcSinPrint(Vi,Vo_var,n,D_var,L,fs))[0]
+            y2[i]=(CalcularCtes.calcSinPrint(Vi,Vo_var,n,D_var,L,fs))[1]
+#            print("Vo_var=",x[i])
+#            print("I_d=",y1[i])
+#            print("I_o=",y2[i],"\n")
+#            print(cte_graf1,Vo_var,D_var)
+            Vo_var = Vo_var+Vo_step #Nuevo paso para próxima iteración.
+        #El siguiente bloque incluye el gráfico segun el desfase porcentual y el valor MODO.
+        if(j==0 and MODO==1):#      (phi=18° => D=0.1) Limite Gef=1.2 en MODO=1.
+            plt.plot(x,y1,'bs-',linewidth=2.0,label=r'$Id(\phi='+str(round(D_var*180,0))+')$')
+            plt.plot(x,y2,'bo-',linewidth=2.0,label=r'$Io(\phi='+str(round(D_var*180,0))+')$')
+        elif(j==1 and MODO==1):#    (phi=27° => D=0.15) Limite Gef=1.4 en MODO=1.
+            plt.plot(x,y1,'gs-',linewidth=2.0,label=r'$Id(\phi='+str(round(D_var*180,0))+')$')
+            plt.plot(x,y2,'go-',linewidth=2.0,label=r'$Io(\phi='+str(round(D_var*180,0))+')$')
+        elif(j==2 and MODO==1):#    (phi=36° => D=0.2) Limite Gef=1.6 en MODO=1.
+            plt.plot(x,y1,'rs-',linewidth=2.0,label=r'$Id(\phi='+str(round(D_var*180,0))+')$')
+            plt.plot(x,y2,'ro-',linewidth=2.0,label=r'$Io(\phi='+str(round(D_var*180,0))+')$')
+#        elif(j==3):#    (phi=45° => D=0.25) Valor intermedio de exigencia.
+#            plt.plot(x,y1,'rs-',linewidth=2.0,label=r'$Id(\phi='+str(round(D_var*180,0))+')$')
+#            plt.plot(x,y2,'ro-',linewidth=2.0,label=r'$Io(\phi='+str(round(D_var*180,0))+')$')
+        elif(j==4 and MODO==2):#    (phi=54° => D=0.3) Valor con elevada exigencia, no maximo.
+            plt.plot(x,y1,'bs-',linewidth=2.0,label=r'$Id(\phi='+str(round(D_var*180,0))+')$')
+            plt.plot(x,y2,'bo-',linewidth=2.0,label=r'$Io(\phi='+str(round(D_var*180,0))+')$')
+#        elif(j==5):#    (phi=63° => D=0.35) Valor con elevada exigencia, no maximo.
+#            plt.plot(x,y1,'ks-',linewidth=2.0,label=r'$Id(\phi='+str(round(D_var*180,0))+')$')
+#            plt.plot(x,y2,'ko-',linewidth=2.0,label=r'$Io(\phi='+str(round(D_var*180,0))+')$')
+        elif(j==6 and MODO==2):#    (phi=72° => D=0.4) Maximo valor de exigencia "justificada".
+            plt.plot(x,y1,'gs-',linewidth=2.0,label=r'$Id(\phi='+str(round(D_var*180,0))+')$')
+            plt.plot(x,y2,'go-',linewidth=2.0,label=r'$Io(\phi='+str(round(D_var*180,0))+')$')
+#        elif(j==7):#    (phi=81° => D=0.45) Valor con elevada exigencia "injustificada".
+#            plt.plot(x,y1,'ys-',linewidth=2.0,label=r'$Id(\phi='+str(round(D_var*180,0))+')$')
+#            plt.plot(x,y2,'yo-',linewidth=2.0,label=r'$Io(\phi='+str(round(D_var*180,0))+')$')
+        elif(j==8 and MODO==2):# (phi=90° => D=0.5) Maximo valor de exigencia "injustificada".
+            plt.plot(x,y1,'rs-',linewidth=2.0,label=r'$Id(\phi='+str(round(D_var*180,0))+')$')
+            plt.plot(x,y2,'ro-',linewidth=2.0,label=r'$Io(\phi='+str(round(D_var*180,0))+')$')
+        Vo_var=Vo_inf       #Se reinicializa el valor del voltaje minimo a graficar.
+        D_var=D_var+D_step  #Se incrementa el desfase porcentual para proxima gráfica.
+    plt.title("fs = "+str(fs*0.001)+" [kHz]    L = "+str(L*1e6)+" [uHy]")
     plt.legend()
     plt.grid(True)
-    plt.xlabel('Voltaje Salida [V]')
-    plt.ylabel('Corriente Eficaz Inductor [A]')
+
+    if(MODO==1):
+        plt.axvline(x=110)
+        plt.axvline(x=138)
+        plt.axvline(x=158)
+        plt.axvline(x=182)
+        major_ticks_Id_Io = np.arange(2,10,1)
+        major_ticks_Vo = np.arange(100,204,4)
+        ax.set_xticks(major_ticks_Vo)
+        ax.set_yticks(major_ticks_Id_Io)
+    elif(MODO==2):
+        plt.axvline(x=Vo_max)
+        major_ticks_Id_Io = np.arange(6,17,1)
+        major_ticks_Vo = np.arange(84,224,4)
+        ax.set_xticks(major_ticks_Vo)
+        ax.set_yticks(major_ticks_Id_Io)
+    plt.xlabel('Tensión de salida [V]')
+    plt.ylabel('Corriente eficaz en cada MOSFET Id [A]\nCorriente media de salida Io [A]')
     plt.show()
 #    print("Vector x = \n",x)
 #    print("Vector y = \n",y)
